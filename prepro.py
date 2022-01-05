5aeb3dd0ef132b92cab587a0a70b184c04becf34
CallessCaller <pht0639@gmail.com>
2022-01-05 12:15:38 +0000

    Park

--- prepro.py
+++ prepro.py
@@ -10,10 +10,13 @@ import json
 import numpy as np
 import pandas as pd
 import pickle
+import random
 from toolz.sandbox import unzip
 
-annotPATH = '/mnt3/user16/vcr/vcr1annots/'
-imagePATH = '/mnt3/user16/vcr/vcr1images/'
+# annotPATH = '/mnt3/user16/vcr/vcr1annots/'
+# imagePATH = '/mnt3/user16/vcr/vcr1images/'
+annotPATH = '/home/vcr/vcr1annots/'
+imagePATH = '/home/vcr/vcr1images/'
 
 '''
 def forward(self, batch, task, compute_loss=True):
@@ -58,52 +61,77 @@ uniter needs xyxywha format
 F.kl_div(F.log_softmax(k, 0), F.softmax(k1, 0), reduction="none").mean()
 '''
 
+def random_word(tokens, vocab_range=(999, 30522), mask=103):
+    """
+    Masking some random tokens for Language Model task with probabilities as in
+        the original BERT paper.
+    :param tokens: list of int, tokenized sentence.
+    :param vocab_range: for choosing a random word
+    :return: (list of int, list of int), masked tokens and related labels for
+        LM prediction
+    """
+    output_label = []
 
-class PretrainDataForVCR(Dataset):
-    def __init__(self, data_type='train'):
-        super().__init__()
-        self.data = pd.read_json(path_or_buf=annotPATH + data_type + '.jsonl', lines=True)
-        self.tokenzier = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.tokenzier.max_length = 220
+    for i, token in enumerate(tokens):
+        prob = random.random()
+        # mask token with 15% probability
+        if prob < 0.15:
+            prob /= 0.15
 
-    def __len__(self):
-        return len(self.data)
+            # 80% randomly change token to mask token
+            if prob < 0.8:
+                tokens[i] = mask
 
-    def __getitem__(self, index):
-        question = self.tokenzier(self.data.question_orig[index], return_token_type_ids=False)
-        answer = self.tokenzier(self.data.answer_orig[index], return_token_type_ids=False)
-        rationale = self.tokenzier(self.data.rationale_orig[index], return_token_type_ids=False)
-        # type_id
-        # 0 -- question
-        # 1 -- region
-        # 2 -- answer
-        # 3 -- rationale
-        tokenzied = question['input_ids'] + answer['input_ids'][1:] + rationale['input_ids'][1:]
-        
-        token_type_ids = [0 for _ in range(len(question['input_ids']))] + [2 for _ in range(len(answer['input_ids'][1:]))] + [3 for _ in range(len(rationale['input_ids'][1:]))]
+            # 10% randomly change token to random token
+            elif prob < 0.9:
+                tokens[i] = random.choice(list(range(*vocab_range)))
 
-        # with open(imagePATH + self.data.metadata_fn[index], 'r') as f:
-        #     meta = json.load(f)
-        with open(imagePATH + self.data.metadata_fn[index][:-5] + '.pickle', 'rb') as f:
-            feature = pickle.load(f)
-        
-        roi_feature = feature['roi_features']
-        nb = roi_feature.shape[1]
-        attention_mask = [1 for _ in range(nb)] + question['attention_mask'] + answer['attention_mask'][1:] + rationale['attention_mask'][1:]
+            # -> rest 10% randomly keep current token
 
-        width = feature['normalized_boxes'][:,:,2] - feature['normalized_boxes'][:,:,0]
-        height = feature['normalized_boxes'][:,:,3] - feature['normalized_boxes'][:,:,1]
-        a = width * height
+            # append current token to output (we will predict these later)
+            output_label.append(token)
+        else:
+            # no masking token (will be ignored by loss function later)
+            output_label.append(-1)
+    if all(o == -1 for o in output_label):
+        # at least mask 1
+        output_label[0] = tokens[0]
+        tokens[0] = mask
 
-        pos = torch.cat((feature['normalized_boxes'], width.unsqueeze(-1)), dim=-1)
-        pos = torch.cat((pos, height.unsqueeze(-1)), dim=-1)
-        pos = torch.cat((pos, a.unsqueeze(-1)), dim=-1)
+    return tokens, output_label
 
-        return (torch.FloatTensor(tokenzied),
-                torch.FloatTensor(token_type_ids),
-                torch.FloatTensor(attention_mask),
-                torch.FloatTensor(roi_feature).squeeze(0),
-                torch.FloatTensor(pos).squeeze(0))
+
+def _get_img_mask(mask_prob, num_bb):
+    img_mask = [random.random() < mask_prob for _ in range(num_bb)]
+    if not any(img_mask):
+        # at least mask 1
+        img_mask[random.choice(range(num_bb))] = True
+    img_mask = torch.tensor(img_mask)
+    return img_mask
+
+def _get_img_tgt_mask(img_mask, txt_len):
+    z = torch.zeros(txt_len, dtype=torch.uint8)
+    img_mask_tgt = torch.cat([z, img_mask], dim=0)
+    return img_mask_tgt
+
+def _get_feat_target(img_feat, img_masks):
+    img_masks_ext = img_masks.unsqueeze(-1).expand_as(img_feat)  # (n, m, d)
+    feat_dim = img_feat.size(-1)
+    feat_targets = img_feat[img_masks_ext].contiguous().view(
+        -1, feat_dim)  # (s, d)
+    return feat_targets
+
+def _mask_img_feat(img_feat, img_masks):
+    img_masks_ext = img_masks.unsqueeze(-1).expand_as(img_feat)
+    img_feat_masked = img_feat.data.masked_fill(img_masks_ext, 0)
+    return img_feat_masked
+
+def _get_targets(img_masks, img_soft_label):
+    soft_label_dim = img_soft_label.size(-1)
+    img_masks_ext_for_label = img_masks.unsqueeze(-1).expand_as(img_soft_label)
+    label_targets = img_soft_label[img_masks_ext_for_label].contiguous().view(
+        -1, soft_label_dim)
+    return label_targets
 
 def pad_tensors(tensors, lens=None, pad=0):
     """B x [T, ...]"""
@@ -131,21 +159,98 @@ def get_gather_index(txt_lens, num_bbs, batch_size, max_len, out_size):
     return gather_index
 
 
+## Here to start
+class PretrainDataForVCR(Dataset):
+    def __init__(self, data_type='train'):
+        super().__init__()
+        self.data = pd.read_json(path_or_buf=annotPATH + data_type + '.jsonl', lines=True)
+        self.tokenzier = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.tokenzier.max_length = 220
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        question = self.tokenzier(self.data.question_orig[index], return_token_type_ids=False)
+        answer = self.tokenzier(self.data.answer_orig[index], return_token_type_ids=False)
+        rationale = self.tokenzier(self.data.rationale_orig[index], return_token_type_ids=False)
+        # type_id
+        # 0 -- question
+        # 1 -- region
+        # 2 -- answer
+        # 3 -- rationale
+        
+        # masking for mlm
+        masked_token_q, txt_label_q = random_word(question['input_ids'][1:-1])
+        masked_token_a, txt_label_a = random_word(answer['input_ids'][1:-1])
+        masked_token_r, txt_label_r = random_word(rationale['input_ids'][1:-1])
+        
+        maksed_tokenzied = [101] + masked_token_q + [102] + masked_token_a + [102] + masked_token_r + [102]
+        txt_label = [-1] + txt_label_q + [-1] + txt_label_a + [-1] + txt_label_r
+
+        #preprocessing
+        tokenzied = question['input_ids'] + answer['input_ids'][1:] + rationale['input_ids'][1:]
+        
+        token_type_ids = [0 for _ in range(len(question['input_ids']))] + [2 for _ in range(len(answer['input_ids'][1:]))] + [3 for _ in range(len(rationale['input_ids'][1:]))]
+
+        # with open(imagePATH + self.data.metadata_fn[index], 'r') as f:
+        #     meta = json.load(f)
+        with open(imagePATH + self.data.metadata_fn[index][:-5] + '.pickle', 'rb') as f:
+            feature = pickle.load(f)
+        
+        roi_feature = feature['roi_features']
+        softlabel = feature['softlabels']
+        nb = roi_feature.shape[1]
+        attention_mask = [1 for _ in range(nb)] + question['attention_mask'] + answer['attention_mask'][1:] + rationale['attention_mask'][1:]
+
+        width = feature['normalized_boxes'][:,:,2] - feature['normalized_boxes'][:,:,0]
+        height = feature['normalized_boxes'][:,:,3] - feature['normalized_boxes'][:,:,1]
+        a = width * height
+
+        pos = torch.cat((feature['normalized_boxes'], width.unsqueeze(-1)), dim=-1)
+        pos = torch.cat((pos, height.unsqueeze(-1)), dim=-1)
+        pos = torch.cat((pos, a.unsqueeze(-1)), dim=-1)
+
+        
+        # for mrfr, mrc
+        img_mask = _get_img_mask(0.15, nb)
+        img_mask_tgt = _get_img_tgt_mask(img_mask, len(tokenzied))
+        #print(roi_feature.squeeze(0).type(),pos.squeeze(0).type(),softlabel.squeeze(0).type(),img_mask.type(),img_mask_tgt.type())
+        
+        return (torch.Tensor(tokenzied),
+                torch.Tensor(token_type_ids),
+                torch.Tensor(attention_mask),
+                roi_feature.squeeze(0),
+                pos.squeeze(0),
+                softlabel.squeeze(0),
+                torch.Tensor(maksed_tokenzied),
+                torch.Tensor(txt_label),
+                img_mask.float(),
+                img_mask_tgt.float())
+
+
 def collate(batch):
-    (input_ids, txt_type_ids, attn_masks, img_feat, img_pos) = map(list, unzip(batch))
-    for i in range(3):
-        print(img_feat[i].shape)
-        print(img_pos[i].shape)
-        print(input_ids[i].shape)
+    (input_ids, txt_type_ids, attn_masks, img_feat, img_pos, softlabel, maksed_tokenzied, txt_label, img_mask, img_mask_tgt) = map(list, unzip(batch))
 
     txt_lens = [i.size(0) for i in input_ids]
     input_ids = pad_sequence(input_ids, batch_first=True, padding_value=0)
     txt_type_ids = pad_sequence(txt_type_ids, batch_first=True, padding_value=0)
     position_ids = torch.arange(0, input_ids.size(1), dtype=torch.long).unsqueeze(0)
+
+    maksed_tokenzied = pad_sequence(maksed_tokenzied, batch_first=True, padding_value=0)
+    txt_label = pad_sequence(txt_label, batch_first=True, padding_value=-1)
     
     num_bbs = [f.size(0) for f in img_feat]
+    softlabel = pad_tensors(softlabel, num_bbs)
     img_feat = pad_tensors(img_feat, num_bbs)
     img_pos = pad_tensors(img_pos, num_bbs)
+    img_mask = pad_sequence(img_mask, batch_first=True, padding_value=0)
+    img_mask = img_mask.bool()
+    label_targets = _get_targets(img_mask, softlabel)
+    feat_target = _get_feat_target(img_feat, img_mask)
+    img_mask_tgt = pad_sequence(img_mask_tgt, batch_first=True, padding_value=0)
+
+    masked_img_feat = _mask_img_feat(img_feat, img_mask)
 
     attn_masks = pad_sequence(attn_masks, batch_first=True, padding_value=0)
 
@@ -153,7 +258,6 @@ def collate(batch):
     out_size = attn_masks.shape[1]
 
     gather_index = get_gather_index(txt_lens, num_bbs, bs, max_tl, out_size)
-    print(gather_index)
 
     batch = {'input_ids': input_ids,
              'txt_type_ids': txt_type_ids,
@@ -161,15 +265,20 @@ def collate(batch):
              'img_feat': img_feat,
              'img_pos_feat': img_pos,
              'attn_masks': attn_masks,
-             'gather_index': gather_index}
+             'gather_index': gather_index,
+             'masked_input_ids': maksed_tokenzied,
+             'txt_label': txt_label,
+             'label_targets': label_targets,
+             'masked_img_feat': masked_img_feat,
+             'feat_targets': feat_target}
 
     return batch
 
 
-dataset = PretrainDataForVCR(data_type='val')
-dataloader = DataLoader(dataset, batch_size=3, shuffle=True, collate_fn=collate)
+# dataset = PretrainDataForVCR(data_type='val')
+# dataloader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=collate)
 
-for i, batch in enumerate(dataloader):
-    print('New Batch! ', i)
-    #print(batch)
-    if i == 2: break
+# for i, batch in enumerate(dataloader):
+#     print('New Batch! ', i)
+#     print(batch)
+#     if i == 2: break
--- pretrain_vcr.py
+++ pretrain_vcr.py
@@ -1,27 +1,99 @@
 import torch
 import torch.nn as F
+import torch.cuda.amp as amp
+import torch.cuda.amp.autocast_mode
+
 from model.pretrain_vcr import UniterForPretrainingForVCR
-from transformers import BertTokenizer
+from prepro import PretrainDataForVCR, DataLoader, collate
+from transformers import AdamW, get_linear_schedule_with_warmup
 
 import json
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 
-annotPATH = '/home/vcr/vcr1annots/'
-imagePATH = '/home/vcr/vcr1images/'
+from torch.utils.tensorboard import SummaryWriter
 
-'''
-def forward(self, batch, task, compute_loss=True):
-        batch = defaultdict(lambda: None, batch)
-        input_ids = batch['input_ids']
-        position_ids = batch['position_ids']
-        img_feat = batch['img_feat']
-        img_pos_feat = batch['img_pos_feat']
-        attention_mask = batch['attn_masks']
-        gather_index = batch['gather_index']
-        txt_type_ids = batch['txt_type_ids']
-'''
+# random seed
+torch.random.manual_seed(42)
 
-train = pd.read_json(path_or_buf=annotPATH + 'train.jsonl', lines=True)
-val = pd.read_json(path_or_buf=annotPATH + 'val.jsonl', lines=True)
+# config 
+batch_size = 128 #6144
+val_batch_size = 8000,
+num_train_steps = 10 #45000
+warmup_steps = 4500
+accum_steps = 1
+valid_steps = 2000
+learning_rate = 3e-05
+
+# dataloader
+train_dataset = PretrainDataForVCR(data_type='train')
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate,
+                              prefetch_factor=5, num_workers=5)
+val_dataset = PretrainDataForVCR(data_type='val')
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate,
+                              prefetch_factor=5, num_workers=5)
+
+# model
+checkpoint = torch.load('pretrained/uniter-base.pt')
+model = UniterForPretrainingForVCR.from_pretrained('config/uniter-base.json', checkpoint, img_dim=2048, img_label_dim=1601)
+model.cuda()
+model.train()
+
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+    ]
+
+
+# optimizer
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+scheduler = get_linear_schedule_with_warmup(optimizer, 4500, num_train_steps)
+scaler = amp.GradScaler()
+loss_sum = 0
+accum = 0
+
+
+current_step = 0
+
+breakValue = False
+
+for epoch in range(100):
+        for i, batch in enumerate(tqdm(train_dataloader)):
+                task_prob = torch.rand(1)
+                if task_prob > 0.66:
+                        task = 'mlm'
+                elif task_prob > 0.33:
+                        task = 'mrc'
+                else:
+                        task = 'mrfr'
+
+                with amp.autocast():
+                        loss = model(batch, task=task, compute_loss=True)
+                        loss = loss.mean()
+
+                scaler.scale(loss).backward()
+
+                loss_sum += loss.item()
+                accum += 1
+
+                if accum != accum_steps:
+                        continue
+                
+                scaler.step(optimizer)
+                scheduler.step()
+                scaler.update()
+                optimizer.zero_grad()
+
+                accum = 0
+                current_step += 1
+
+                if current_step == num_train_steps: 
+                        breakValue = True
+                        break
+        if breakValue:
+                break
+
 
